2b6eb1626844cb5aeeb36b6a2e570f25df5aef02
CaioKfuri <caioakfuri@gmail.com>
2024-06-24 19:39:23 -0300

    Criação e códigos iniciais

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <link rel="shortcut icon" href="./resources/person.png" type="image/x-icon">
+    <title>Clientes</title>
+    <style type="text/css">
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+        body{
+            margin: 20px;
+            background-color: white;
+        }
+        *{
+            font-family: 'Opens Sans', sans-serif;
+        }
+        h2{
+            font-family: 'Opens Sans', sans-serif;
+        }
+
+        .thead{
+            background-color: #f7f7f7;
+        }
+    </style>
+
+</head>
+<body>
+    
+    <div class="container">
+        <h2 class="text-center"> Lista de Clientes <i class="bi bi-people-fill"></i></h2>
+
+        <h5 class="text-end">
+            <a href="view/page_register.php" class="btn btn-primary btn-xs">
+                <i class="bi bi-person-plus-fill"></i>
+            </a>
+        </h5>
+    </div>
+
+    
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+</body>
+</html>
